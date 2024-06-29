a996a7cbaa9940716fe021cf82a0e94f3be4ef70
Matthew Wanjiku <imattheaven@gmail.com>
2024-06-29 13:54:45 +0300

    Zero precedence for values less than 10

--- script.js
+++ script.js
@@ -14,11 +14,25 @@ function stopWatch() {
         } else {
             minutes = 0;
             hours+=1;
-        }
-        displayTime.innerHTML = `${hours} : ${minutes} : ${seconds}`;
-        
+        }        
         
         console.log(hours, minutes, seconds);
+
+        display(seconds, minutes, hours);
     }, 1000);
 }
 
+
+function display(seconds, minutes, hours) {
+    if(seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    if(minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    
+    displayTime.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+}
